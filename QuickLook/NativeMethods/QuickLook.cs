0755409a30953d4d11be6ebd8e03721012e63256
ema <mccoy39082@163.com>
2024-05-25 19:50:03 +0800

    Change QuickLook.csproj to .NET SDK type

--- QuickLook/NativeMethods/QuickLook.cs
+++ QuickLook/NativeMethods/QuickLook.cs
@@ -1,17 +1,17 @@
 ﻿// Copyright © 2017 Paddy Xu
-// 
+//
 // This file is part of QuickLook program.
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -113,9 +113,9 @@ namespace QuickLook.NativeMethods
             if (Path.GetExtension(path).ToLower() != ".lnk") return path;
 
             var link = new ShellLink();
-            ((IPersistFile) link).Load(path, 0);
+            ((IPersistFile)link).Load(path, 0);
             var sb = new StringBuilder(MaxPath);
-            ((IShellLinkW) link).GetPath(sb, sb.Capacity, out _, 0);
+            ((IShellLinkW)link).GetPath(sb, sb.Capacity, out _, 0);
 
             return sb.Length == 0 ? path : sb.ToString();
         }
